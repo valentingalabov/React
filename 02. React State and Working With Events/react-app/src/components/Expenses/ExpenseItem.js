c0c3ed2123606d01d88af4d4314cc43ada5fd5a8
v_galabow@abv.bg <v_galabow@abv.bg>
2021-10-17 19:42:56 +0300

    React state and working with events.

---
+++ 02. React State and Working With Events/react-app/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickedHandler = () => {
+    setTitle('Updated!');
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickedHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
