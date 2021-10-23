7a791969d835fcfbebb363d3de88028380176bb5
v_galabow@abv.bg <v_galabow@abv.bg>
2021-10-23 13:50:31 +0300

    Initial create of Food Order App and implementation of meals and header fragments.

---
+++ 08. Practice - Building a Food Order App/react-foodOrder-app/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+import HeaderCardButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
